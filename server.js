01eb59295ce05e93dd69fd6940444a6a92bd608d
Chiragraj Sen <114171989+SenChiragraj@users.noreply.github.com>
2025-04-26 18:31:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 // Initialize Express app
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 dotenv.config();
 
